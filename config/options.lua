5a04e5c84c0cbfaac20c137c5fde7442c47a2ef9
embedded <yidazou.mail@gmail.com>
2025-11-01 23:50:42 +0800

    设置默认缩进4空格，移除多余设置

--- config/options.lua
+++ config/options.lua
@@ -2,8 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Use system clipboard (requires xclip or xsel on Linux)
-vim.opt.clipboard = "unnamedplus"
+-- Use system clipboard requires xclip or xsel on Linux
 
 -- Disable auto format on save
 vim.g.autoformat = false
+
+-- Set default indentation to 4 spaces
+vim.opt.tabstop = 4        -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 4     -- Number of spaces to use for each step of (auto)indent
