e59bb79f43d43db3559d8ffe98d846c725d120b8
Yida Zou <zou_yida@outlook.com>
2025-11-15 18:21:34 +0800

    Add some tips for system clipboard

--- config/options.lua
+++ config/options.lua
@@ -2,7 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Use system clipboard requires xclip or xsel on Linux
+-- Use system clipboard requires xclip or wl-clipboard on Linux
+-- See :h clipboard for more infomation
 
 -- Disable auto format on save
 vim.g.autoformat = false
