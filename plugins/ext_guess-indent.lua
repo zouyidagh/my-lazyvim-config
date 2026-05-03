14eec41e4a03eacd25975cac504fb94b8e0313c9
Yida Zou <3230063239@qq.com>
2026-05-03 19:23:09 +0800

    新增外部guess-indent.nvim插件，整理plugins目录下文件

---
+++ plugins/ext_guess-indent.lua
@@ -0,0 +1,7 @@
+-- This is an external plugin not included in LazyVim
+return {
+  "nmac427/guess-indent.nvim",
+  opts = {
+    override_editorconfig = true -- Set to true to override settings set by .editorconfig
+  },
+}
